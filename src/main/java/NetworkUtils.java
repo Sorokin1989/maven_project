5bb86a8e9a5f4465c96ee679df36656f4fe31790
Dmitrii <sorokindm1try@yandex.ru>
2025-02-27 16:41:44 +0300

    junit 1.2

--- src/main/java/NetworkUtils.java
+++ src/main/java/NetworkUtils.java
@@ -1,3 +1,9 @@
 public class NetworkUtils {
 
+public static void getConnection() {
+    //получаем соединение с сервером
+
+
+    return;
+}
 }
---
+++ src/test/java/NetworkUtilTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class NetworkUtilTest {
+    @Test(timeout = 1000)
+    public void getConnectionShouldReturnFasterThanOneSecond() {
+        NetworkUtils.getConnection();
+    }
+
+}
